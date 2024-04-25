b6627c43faa475abcb39e83bf97178f3fa6b5419
Maslov Maxim <128380279+wh1t3tea@users.noreply.github.com>
2024-04-26 02:54:10 +0300

    Delete cfg.py

--- cfg.py
+++
@@ -1,7 +0,0 @@
-from model import DurationPredictor
-import torch
-from torch import optim
-from torch.optim import lr_scheduler
-from dataset import PhoneDataset
-from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
